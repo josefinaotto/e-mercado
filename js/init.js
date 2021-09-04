6bc1c672ee15ace08a9ef1ab4c1665fc0e9a40ea
josefinaotto <josefinaotto@gmail.com>
2021-09-04 12:01:02 +0200

    Update init.js

--- js/init.js
+++ js/init.js
@@ -44,4 +44,9 @@ var getJSONData = function(url){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+
+  let idUsuario = document.getElementById("mostrarUsuario");
+  let nombreUsuario = localStorage.getItem("user");
+  idUsuario.innerHTML = "Mail: " + nombreUsuario; 
+
 });
