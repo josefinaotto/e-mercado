f07c630a77d68e85fddd50a07f4061d7d51ca0f3
josefinaotto <josefinaotto@gmail.com>
2021-10-31 14:16:52 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -7,51 +7,56 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
-var showSpinner = function(){
+var showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
 }
 
-var hideSpinner = function(){
+var hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-var getJSONData = function(url){
-    var result = {};
-    showSpinner();
-    return fetch(url)
+var getJSONData = function (url) {
+  var result = {};
+  showSpinner();
+  return fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
+    .then(function (response) {
+      result.status = 'ok';
+      result.data = response;
+      hideSpinner();
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
+    .catch(function (error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
     });
 }
 
 //-------------------------------------------------------------------------------------------------------------------------
 
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
   let idUsuario = document.getElementById("mostrarUsuario");
+  let emailUsuario = document.getElementById("mostrarEmail");
+
   let nombreUsuario = localStorage.getItem("user");
-  idUsuario.innerHTML = nombreUsuario; 
+  
+  idUsuario.innerHTML = nombreUsuario;
+  emailUsuario.innerHTML = nombreUsuario;
 
 
-  document.getElementById("cerrarSesion").addEventListener("click", function() {
+  document.getElementById("cerrarSesion").addEventListener("click", function () {
 
     localStorage.clear();
+
   });
 
 });
