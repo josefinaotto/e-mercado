c1d40368ccd669a351f670046a52c8f55ed316d1
josefinaotto <josefinaotto@gmail.com>
2021-10-31 14:17:21 -0300

    Update my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,31 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+
+function infoUsuario() {
+    let edadIngresada = document.getElementById("age");
+    let nombreIngresado = document.getElementById("name");
+    let telefonoIngresado = document.getElementById("numero");
+
+    localStorage.setItem("edad", edadIngresada.value);
+    localStorage.setItem("nombre", nombreIngresado.value);
+    localStorage.setItem("telefono", telefonoIngresado.value);
+
+    location.reload();
+}
+
+
+
+//-----------------------------------------------------------------------------------------------------------
 document.addEventListener("DOMContentLoaded", function (e) {
+
+    let ageUsuario = document.getElementById("mostrarEdad");
+    let nameUsuario = document.getElementById("mostrarNombre");
+    let numeroUsuario = document.getElementById("mostrarTelefono");
+
+    let edadPersona = localStorage.getItem("edad");
+    let nombreyapellido = localStorage.getItem("nombre");
+    let telefonoPersona = localStorage.getItem("telefono");
+    
+    ageUsuario.innerHTML = edadPersona;
+    nameUsuario.innerHTML = nombreyapellido;
+    numeroUsuario.innerHTML = telefonoPersona; 
 
 });
